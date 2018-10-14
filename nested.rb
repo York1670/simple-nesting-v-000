9fe5198099fd7bd61d3d3429692945752bf3ac6d
Bobbi York <york1670@gmail.com>
2018-10-14 20:00:23 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -96,7 +96,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-  programmer_hash[:changing_alan][:alan_kay][:known_for] = "GUI"
+  programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
